fec5193df8f89cbd7d095649a09339809539d631
DhafindraStefano <Dhafindra.razaqa@gmail.com>
2025-08-21 10:32:52 +0800

    created Tab and Cards view

---
+++ challenge4/Views/Star Details/Cards.swift
@@ -0,0 +1,18 @@
+//
+//  Cards.swift
+//  challenge4
+//
+//  Created by Dhafindra Razaqa Stefano on 21/08/25.
+//
+
+import SwiftUI
+
+struct Cards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Cards()
+}
