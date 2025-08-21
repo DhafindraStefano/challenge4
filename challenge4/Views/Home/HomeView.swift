4f2be63593c814521250f72dfd5334dfe92e04b5
Ardelia <ardeliautomo@gmail.com>
2025-08-21 11:33:48 +0800

    checkpoint

--- challenge4/Views/Home/HomeView.swift
+++ challenge4/Views/Home/HomeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct HomeView: View {
     @State private var daysCount : Int = 0
@@ -13,23 +14,33 @@ struct HomeView: View {
     var body: some View {
         NavigationStack{
             ZStack{
-                //Need to figure out how to scatter the stars and the Moon
+                //Need to figure out how to scatter the stars and the Moon, first layer
+                
                 VStack{
-                    Spacer()
+                    
                     Image("StarHome")
-                    Image("FullMoon")
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                }
-                
-                
-                VStack {
-                    Spacer()
-                    Image("MoonHome")
                         .resizable()
                         .scaledToFit()
+                        .frame(width: 45, height: 79)
+                        .offset(x:-110,y:-70)
+                    Image("FullMoon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 79, height: 79)
+                        .offset(x:120,y:-189)
                 }
+                
+                //Second Layer
+                VStack {
+                    Image("MoonBase")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 450)
+                        .offset(y:300)
+                        .ignoresSafeArea(.all)
+                }
+                
+                //Third Layer
                 VStack{
                     HStack{
                         ZStack {
@@ -37,14 +48,13 @@ struct HomeView: View {
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 177, height: 65)
-//                            The distance between the stetoscope and the text supposed to be 7px but Ardel haven't figured out how to do that. So
+
                             HStack(spacing: 0) {
-                                Spacer()
-                                Spacer()
                                 Text ("\(daysCount)")
                                     .font(.system(size:28, design: .rounded ))
                                     .fontWeight(.bold)
                                     .foregroundStyle(.white)
+                                    .offset(x: 58, y:-6)
                                 Text("\\\\\(daysTotal)")
                                     .font(.system(size:28, design: .rounded ))
                                     .fontWeight(.bold)
@@ -52,7 +62,7 @@ struct HomeView: View {
                                     .foregroundStyle(
                                         .white
                                         .opacity(0.3))
-                                Spacer()
+                                    .offset(x: 58, y:-6)
                             }.frame(maxWidth:148, alignment: .topLeading)
                         }
                         Spacer()
@@ -64,11 +74,28 @@ struct HomeView: View {
                     }.padding(.horizontal, 20.0)
                     
                     HStack(spacing:-50){
-                            Image("MotherBunny")
+                        ZStack {
+                            Image("ChildStone")
                                 .resizable()
                                 .scaledToFit()
-                                .frame(width: 163, height: 222)
-                                .offset(y:280)
+                                .frame(width: 210.32, height: 69)
+                                .offset(x:130,y:365)
+                            LottieView(name: "rabbit talk child", // the name is the name of the .json file
+                                       loopMode: .loop, contentMode: .scaleAspectFit, speed: 1.0)
+                            .frame(width: 134, height: 173)
+                            .offset(x:500,y: 2200)
+                            .scaleEffect(0.13)
+                            Image("ParentStone")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 99.84, height: 65.69)
+                                .offset(x:-30,y:365)
+                        }
+                        
+                        
+//                            .scaleEffect(1, anchor: .trailing)
+            
+
 
                             Image("ChildBunny")
                                 .resizable()
@@ -78,19 +105,19 @@ struct HomeView: View {
                         
                         
                     }
-                    Spacer()
+                    
                     Button(){
                         
                     }label:{
                         Image("StartTalkBtn")
-                            .offset( y:140 )
+                            .offset(x:10, y:320 )
                     }
                     Spacer()
                 }
                
                 
             }
-//            .background(Color("AppBg"))
+            .background(Color("AppBg"))
     
         }
     }
