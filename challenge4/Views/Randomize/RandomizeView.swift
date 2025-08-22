3316c9f278b6588cdd9ae939605188449f4220be
Felisha Levana <felishaleevana@gmail.com>
2025-08-22 08:34:45 +0800

    Squashed commit of the following:
    commit 47cdaf22a6d89a1b159e006fe3e708842cd12e84
    Author: DhafindraStefano <121856385+DhafindraStefano@users.noreply.github.com>
    Date:   Thu Aug 21 22:08:49 2025 +0800
    
        Implemented NVC view (#10)
    
    commit 28b99c2211fa094788f81e32d2beab3420f4af0a
    Author: DhafindraStefano <Dhafindra.razaqa@gmail.com>
    Date:   Thu Aug 21 21:50:30 2025 +0800
    
        new animations and resolving conflicts
    
    commit ecb5cf24074f3885ef3bda54f60fc02f217d3c8e
    Author: sangreal-007 <contact.dariaiap@gmail.com>
    Date:   Thu Aug 21 21:40:25 2025 +0800
    
        Add Randomize views (BackButton, RecordButton, RandomizeView) (#9)

--- challenge4/Views/Randomize/RandomizeView.swift
+++ challenge4/Views/Randomize/RandomizeView.swift
@@ -1,15 +1,83 @@
 //
-//  RandomizeView.swift
+//  StoryView.swift
 //  challenge4
-//
-//  Created by Dhafindra Razaqa Stefano on 19/08/25.
 //
 
 import SwiftUI
 
 struct RandomizeView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
+                
+                VStack {
+                    VStack (spacing: 20) {
+                        Text("Can you tell me a story\nabout your childhood?")
+                            .font(Font.custom("SF Pro Rounded", size: 28))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                        
+                        HStack(spacing: 8) {
+                            Text("Randomize")
+                                .font(Font.custom("SF Pro Rounded", size: 20))
+                                .kerning(0.4)
+                                .foregroundColor(Color(red: 0.46, green: 0.45, blue: 1))
+                            
+                            Image(systemName: "dice")
+                                .font(.system(size: 20))
+                                .foregroundColor(Color(red: 0.46, green: 0.45, blue: 1))
+                        }
+                    }
+                    
+                    ZStack {
+                        Image("Moon")
+                            .resizable()
+                            .scaledToFit()
+                            .offset(x: 0, y: 251)
+                        
+                        Image("StoneBig")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 110)
+                            .offset(x: -100, y: 160)
+                        
+                        Image("StoneWide")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 240)
+                            .offset(x: 80, y: 160)
+                        
+                        LottieView(name: "rabbit talk mom", // the name is the name of the .json file
+                                   loopMode: .loop, contentMode: .scaleAspectFit, speed: 1.0)
+                                .frame(width: 283, height: 345)
+                                .offset(x: -300, y: 50)
+                                .scaleEffect(0.2)
+                        
+                        LottieView(name: "rabbit talk child", // the name is the name of the .json file
+                                   loopMode: .loop, contentMode: .scaleAspectFit, speed: 1.0)
+                                .frame(width: 200, height: 300)
+                                .offset(x: -100, y: 50)
+                                .scaleEffect(0.15)
+                        
+                        RandomizeRecordButton()
+                            .scaleEffect(1.1)
+                            .offset(x: 0, y: 272)
+                    }
+                }
+            }
+            .overlay(alignment: .topLeading) {
+                BackButton()
+                .padding(.top, 20)
+                .padding(.leading, 20)
+                .scaleEffect(1.2)
+                .zIndex(10)
+            }
+        }
+        .navigationBarBackButtonHidden(true)
     }
 }
 
